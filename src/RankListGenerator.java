261a8ddde423267dc41eec35affbb297af3f6f53
prakash wekancode <prakash@wekancode.com>
2015-03-11 16:36:55 +0530

    adding UI changes

--- src/RankListGenerator.java
+++ src/RankListGenerator.java
@@ -36,7 +36,7 @@ public class RankListGenerator {
 	String getContent(List<Submission> submissions){
 		Calendar calendar = GregorianCalendar.getInstance();		
 		String answer = "Last updated at "+calendar.get(Calendar.HOUR_OF_DAY)+":"+calendar.get(Calendar.MINUTE)+" IST\n\n";
-		answer += "<table style=\"width:100%\">";
+		answer += "<table style=\"width:100%; border-spacing: 8px;\">";
 		answer += "<colgroup>";
 		answer += "<col span=\"1\" style =\"width: 10%;\">";
 		answer += "<col span=\"1\" style =\"width: 25%;\">";
@@ -47,12 +47,12 @@ public class RankListGenerator {
 		answer += "</colgroup>";
 		answer += "<thead>";
 		answer+= "<tr>";
-		answer += "<th>";  answer += "Rank"; answer += "</th>";
+		answer += "<th style=\"text-align:center\";>";  answer += "Rank"; answer += "</th>";
 		answer += "<th>";  answer += "Name"; answer += "</th>";
 		for(int i=0;i<Constants.problemNames.length;i++){
-			answer += "<th>";  answer += Constants.problemNames[i]; answer += "</th>";
+			answer += "<th style=\"text-align:center\";>";  answer += Constants.problemNames[i]; answer += "</th>";
 		}
-		answer += "<th>";  answer += "Points"; answer += "</th>";
+		answer += "<th style=\"text-align:center\";>";  answer += "Points"; answer += "</th>";
 		answer += "<tr/>";		
 		answer += "</thead>";
 		
@@ -60,15 +60,15 @@ public class RankListGenerator {
 		answer += "<tbody>";
 		for(int i=0; i<submissions.size(); i++){
 			answer += "<tr>";
-			answer += "<td>"; answer += String.valueOf(i+1); answer +="</td>";
+			answer += "<td style=\"text-align:center\";>"; answer += String.valueOf(i+1); answer +="</td>";
 			String name = IDs.containsKey(submissions.get(i).email)? participants.get((int) IDs.get(submissions.get(i).email)).name : "Unknown";
 			answer += "<td>"; answer += name; answer +="</td>";
 			int points = 0;
 			for(int j=0;j<Constants.problemNames.length;j++){
-				answer += "<td bgcolor=\""; answer+=submissions.get(i).didSolve(j+1)?Constants.GREEN:Constants.WHITE; answer +="\"></td>";
+				answer += "<td style=\"text-align:center\"; bgcolor=\""; answer+=submissions.get(i).didSolve(j+1)?Constants.GREEN:Constants.WHITE; answer +="\"></td>";
 				points += submissions.get(i).didSolve(j+1)? 3 : 0;
 			}
-			answer += "<td>"; answer += String.valueOf(points); answer +="</td>";
+			answer += "<td style=\"text-align:center\";>"; answer += String.valueOf(points); answer +="</td>";
 			answer += "<tr/>";
 		}		
 		answer += "</tbody>";
